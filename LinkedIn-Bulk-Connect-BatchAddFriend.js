4411667589e8a1504df2d3a7997f5350aa8defdc
zengfr曾繁荣 <zengfr3000@qq.com>
2019-04-28 21:59:08 +0800

    feat:linkedin批量自动添加好友联系人工具

---
+++ LinkedIn-Bulk-Connect-BatchAddFriend.js
@@ -0,0 +1,55 @@
+--https://github.com/TheWebAuthor/LinkedIn-Bulk-Connect
+--https://github.com/zengfr/LinkedIn-Bulk-Connect-BatchAddFriend
+--https://gitee.com/zengfr/LinkedIn-Bulk-Connect-BatchAddFriend
+function randomNum(minNum,maxNum){ 
+    switch(arguments.length){ 
+        case 1: 
+            return parseInt(Math.random()*minNum+1,10); 
+        break; 
+        case 2: 
+            return parseInt(Math.random()*(maxNum-minNum+1)+minNum,10); 
+        break; 
+            default: 
+                return 0; 
+            break; 
+    } 
+} 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+function loop_clicks(sleepMillisecond) {
+	var count = $('button.search-result__actions--primary').not('button.message-anywhere-button').length;
+	if (count == 0) {
+		next();
+	}
+	else {
+		$('button.search-result__actions--primary').not('button.message-anywhere-button').each( function(index,element) {
+			sleep(index*randomNum(sleepMillisecond,sleepMillisecond*2));
+			click_connect($(this));
+			--count;
+		});
+		if (count<=0) {
+			next();
+		}
+	}
+}
+
+
+function click_connect($obj) {
+	$obj.click();
+	$('button.ml1').click();
+}
+
+var delay = ( function() {
+    var timer = 0;
+    return function(callback, ms) {
+        clearTimeout (timer);
+        timer = setTimeout(callback, ms);
+    };
+})();
+var sleepMillisecond=11*1000;
+function next() {
+	$('button.artdeco-pagination__button--next').click();
+	delay(function(){ loop_clicks(sleepMillisecond); }, sleepMillisecond*15);
+}
+loop_clicks(sleepMillisecond);
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # LinkedIn-Bulk-Connect-BatchAddFriend
-LinkedIn-Bulk-Connect批量添加好友
+LinkedIn-Bulk-Connect-BatchAddFriend www.linkedin.com linkedin批量自动添加好友联系人工具
